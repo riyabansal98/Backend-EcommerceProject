e9135a8828d38f52687aff153d855884ac7a0f86
Riya Bansal <riyabansal@Riyas-MacBook-Pro.local>
2022-11-29 23:15:26 +0530

    Added Sample Tests

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -5,14 +5,30 @@ const MathOperations = require('./sample')
 describe("Calculator tests", () => {
 
     test('addition', () => {
-        expect(MathOperations.sum(1, 2)).toBe(3);
+
+        expect(MathOperations.sum(1, 2)).toBe(3); //positive test case
+        expect(MathOperations.sum(5, 4)).not.toBe(8); //negative test case
         expect(MathOperations.sum(5, 8)).toBe(13);
     })
 
-    test('addition', () => {
-        expect(MathOperations.sum(1, 2)).toBe(3);
-        expect(MathOperations.sum(5, 8)).toBe(13);
+    //Subtraction
+    test('subtraction', () => {
+
+        expect(MathOperations.diff(5, 3)).toBe(2);
+        expect(MathOperations.diff(10, 10)).toBe(0);
+        expect(MathOperations.diff(8, 7)).not.toBe(5);
+
     })
+
+    //product
+    test('product', () => {
+
+        expect(MathOperations.product(5, 3)).toBe(15);
+        expect(MathOperations.product(10, 10)).toBe(100);
+        expect(MathOperations.product(8, 7)).not.toBe(50);
+
+    })
+
 
 })
 
